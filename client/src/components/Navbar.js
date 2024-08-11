fe06618d11e9780740d3da30841e0f12b6433b1c
unknown <andrews_j@live.co.uk>
2024-08-11 18:25:02 +0100

    update styles

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,73 +1,74 @@
-import React, { useState, useEffect } from 'react'
-import '../styles/style.css'
-import { useDispatch } from 'react-redux';
-import { Link, useHistory } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { LOG_OUT } from '../actions/types';
+import React, { useState, useEffect } from "react";
+import "../styles/style.css";
+import { useDispatch } from "react-redux";
+import { Link, useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { LOG_OUT } from "../actions/types";
 
 export default function Navbar() {
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem('person')))
-  
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("person")));
+
   const dispatch = useDispatch();
   const history = useHistory();
-  const logged = useSelector((state) => state.auth.isSignedIn)
+  const logged = useSelector(state => state.auth.isSignedIn);
+  const { items } = useSelector(state => state.cart);
+
+  let bool = items.length > 0;
 
   function logout() {
     dispatch({ type: LOG_OUT });
 
-    history.push('/')
+    history.push("/");
 
     setUser(null);
   }
 
   function navBtn() {
-
     if (user) {
-      logout()
-
+      logout();
     } else {
-      history.push('/auth/signin')
+      history.push("/auth/signin");
     }
   }
-  
+
   useEffect(() => {
-
-    setUser(JSON.parse(localStorage.getItem('person')))
-     
+    setUser(JSON.parse(localStorage.getItem("person")));
   }, [logged]);
-
 
   return (
     <div>
       <nav className="navbar">
-        <span className="user"><h4>{user ? `Logged in as: ${user.data.name}`: 'Not logged in' }</h4>
-          <button className="button" onClick={() => navBtn()}>{user ? 'Logout' : 'Sign in'}</button>
+        <span className="user">
+          <h4>{user ? `Logged in as: ${user.data.name}` : "Not logged in"}</h4>
+          <button className="button" onClick={() => navBtn()}>
+            {user ? "Logout" : "Sign in"}
+          </button>
         </span>
         <ul className="nav">
-          <li><Link to="/">
-            Home
-          </Link>
-          </li>
-          <li><Link to="/cart">
-            Cart
-          </Link>
-          </li>
-          {user ? 
-          <>
           <li>
-            <Link to="/admin">
-              Admin
-            </Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/admin/products">
-              Admin Products
+            <Link to="/cart">
+              <i
+                class="fa fa-shopping-basket"
+                aria-hidden="true"
+                style={{ opacity: bool ? "1" : "" }}
+              ></i>
             </Link>
           </li>
-          </>
-           : null}
+          {user ? (
+            <>
+              <li>
+                <Link to="/admin">Admin</Link>
+              </li>
+              <li>
+                <Link to="/admin/products">Admin Products</Link>
+              </li>
+            </>
+          ) : null}
         </ul>
       </nav>
     </div>
-  )
-};
+  );
+}
--- client/src/components/SuccessNotification.js
+++ client/src/components/SuccessNotification.js
@@ -1,54 +1,64 @@
-import React,{useEffect} from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { hideSuccess } from '../actions/auth';
-import { hideCartSuccess } from '../actions/cart';
-import '../styles/main.css'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { hideSuccess } from "../actions/auth";
+import { hideCartSuccess } from "../actions/cart";
+import "../styles/main.css";
 
 export function SuccessNotification() {
-    const isSignedIn = useSelector(state => state.auth.isSignedIn)
-    const message = useSelector(state => state.auth.success)
-    const itemInCart = useSelector(state => state.cart.success)
-  
+  const isSignedIn = useSelector(state => state.auth.isSignedIn);
+  const message = useSelector(state => state.auth.success);
+  const itemInCart = useSelector(state => state.cart.success);
+
   const dispatch = useDispatch();
-  
-  function handleClose(){
-    dispatch(hideSuccess())
+
+  function handleClose() {
+    dispatch(hideSuccess());
   }
 
-  function cartClose(){
-    dispatch(hideCartSuccess())
+  function cartClose() {
+    dispatch(hideCartSuccess());
   }
   useEffect(() => {
-    function cClose(){
-      dispatch(hideCartSuccess())
+    function cClose() {
+      dispatch(hideCartSuccess());
     }
     const timeout = setTimeout(() => {
-      cClose()
+      cClose();
     }, 5000);
-  
-    return () => {
-      clearTimeout(timeout)
-    }
-  }, [itemInCart, dispatch])
-  
-  if(itemInCart){
 
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [itemInCart, dispatch]);
+
+  if (itemInCart) {
     return (
       <>
-      <article className="message is-success" style={{width:'fit-content', position:"absolute"}}>
-      <span className='message-body'>{itemInCart}</span>
-      <button className='button is-success is-small' onClick={cartClose}>Close</button>
-      </article>
-    </>
-    )
-  } 
+        <article
+          className="message is-success"
+          style={{ width: "fit-content", position: "fixed", zIndex: "2" }}
+        >
+          <span className="message-body">{itemInCart}</span>
+          <button className="button is-success is-small" onClick={cartClose}>
+            Close
+          </button>
+        </article>
+      </>
+    );
+  }
   return (
     <>
-    {isSignedIn && message && (
-      <article className="message is-success" style={{width:'fit-content',position:"absolute"}}>
-      <span className='message-body'>{message}</span>
-      <button className='button is-success is-small' onClick={handleClose}>Close</button>
-      </article>    )}
+      {isSignedIn && message && (
+        <article
+          className="message is-success"
+          style={{ width: "fit-content", position: "absolute" }}
+        >
+          <span className="message-body">{message}</span>
+          <button className="button is-success is-small" onClick={handleClose}>
+            Close
+          </button>
+        </article>
+      )}
     </>
-  )
+  );
 }
